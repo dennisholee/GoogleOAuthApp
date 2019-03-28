b8d1ad0588ed882dd521eab1a41e0dbfad3b0917
pleb <freeradicals123@gmail.com>
2019-03-29 01:00:16 +0800

    Add and comply to eslint config. Add gcp datastore create and find all. Closes #9.

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 var express = require('express')
-var bodyParser = require('body-parser')
-var passport = require('passport');
-var refresh = require('passport-oauth2-refresh')
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-var environment = require('./environments')
-var PropertiesReader = require('properties-reader')
-var axios = require('axios')
+    , bodyParser = require('body-parser')
+    , passport = require('passport')
+    , refresh = require('passport-oauth2-refresh')
+    , GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+    , environment = require('./environments')
+    , PropertiesReader = require('properties-reader')
+    , axios = require('axios')
+    , {Datastore} = require('@google-cloud/datastore')
+    , tokenmanager = require('./tokenmanager.js')
+    , logger = require('./logger.js')
 
-var {Datastore} = require('@google-cloud/datastore')
-
-var tokenmanager = require('./tokenmanager.js')
-
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
 var properties = new PropertiesReader(environment)
 var clientID = properties.get('main.clientID')
@@ -27,43 +26,46 @@ var spreadsheetId = properties.get('google.spreadsheetId')
 var projectName = properties.get('gcp.projectName')
 
 // TODO: Bind accessToken to user session
-var gblAccessToken = ""
+var gblAccessToken = ''
 
-var strategy = new GoogleStrategy({
-    clientID: clientID,
-    clientSecret: clientSecret,
-    callbackURL: callbackURL
-  },
-  function(accessToken, refreshToken, profile, done) {
-    if (profile) {
-	console.log("gblAccessToken: " + gblAccessToken)
-	gblAccessToken = accessToken 
-        user = profile;
+var strategy = new GoogleStrategy(
+    {
+        clientID: clientID,
+        clientSecret: clientSecret,
+        callbackURL: callbackURL
+    },
+    function(accessToken, refreshToken, profile, done) {
+        if (profile) {
+            logger.log('info', `Access Token [token=${gblAccessToken}`)
 
-	// Save refresh token
-	console.log("Preparing to save token to datastore [id=" + profile.id + ", token=" + refreshToken + "]")	
-	axios.post('/tokenstore/token', { 'id' : profile.id, 'token' : refreshToken })
-	    .then(res => {
-	    }).catch(err => {
-	    })
-	create(profile, refreshToken)
+            // TODO: Assign token to session
+            gblAccessToken = accessToken 
+            let user = profile
 
-	return done(null, user);
-    } else {
-        return done(null, false);
+            // Save refresh token
+            logger.log('info', `Preparing to save token to datastore [id=${profile.id}, token=${refreshToken}]`)    
+            axios.post('/tokenstore/token', { 'id' : profile.id, 'token' : refreshToken })
+                .then(res => {
+                }).catch(err => {
+                })
+            create(profile, refreshToken)
+
+            return done(null, user)
+        } else {
+            return done(null, false)
+        }
     }
-  }
 )
 
 passport.use(strategy)
 refresh.use(strategy)
 
 passport.serializeUser(function(user, done) {
-	done(null, user);
+    done(null, user)
 })
 
 passport.deserializeUser(function(user, done) {
-  done(null, user);
+    done(null, user)
 })
 
 var app = express()
@@ -71,134 +73,134 @@ app.use(bodyParser.json())
 app.use(passport.initialize())
 app.use(passport.session())
 app.use((req, res, next) => {
-  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private')
-  next()
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private')
+    next()
 })
-app.use('/tokenstore', tokenmanager);
+app.use('/tokenstore', tokenmanager)
 
 app.get('/', (req, res) => {
-	res.status(200).send('<a href="/auth/google">Sign In with Google</a>')
+    res.status(200).send('<a href="/auth/google">Sign In with Google</a>')
 })
 
 app.get('/private', (req, res) => {
-	axios.get(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}?includeGridData=false`, {
-  		headers: {'Authorization': 'Bearer '+gblAccessToken, 'Accept': 'application/json'}
-	}).then(function (response) {
+    axios.get(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}?includeGridData=false`, {
+        headers: {'Authorization': `Bearer ${gblAccessToken}`, 'Accept': 'application/json'}
+    }).then(response => {
 
-		if(response.status === 403) {
-			res.status(200).send('Private Page 403<br />Access Token: ' + gblAccessToken + '<br />Message: Oh no! Caught 402 Error')
-		} else {
-			console.log("*** response: " + JSON.stringify(response.data))
-			res.status(200).send('Private Page<br />Access Token: ' + gblAccessToken + '<br />Message: ' + JSON.stringify(response.data))
-		}
-	}).catch(function (error) {
-    		console.log("*** error: " + error);
-		//id = get('110836914681755155923')
+        if(response.status === 403) {
+            res.status(200).send('Private Page 403<br />Access Token: ' + gblAccessToken + '<br />Message: Oh no! Caught 402 Error')
+        } else {
+            logger.log('info', `*** response: ${JSON.stringify(response.data)}`)
+            res.status(200).send(`Private Page<br />Access Token: ${gblAccessToken}<br />Message: ${JSON.stringify(response.data)}`)
+        }
+    }).catch(err => {
+        logger.log('info', `*** error: ${err}`)
+        //id = get('110836914681755155923')
 
-		// Add condition to check for error to be 403. At the moment assumption is made.
-		// Get accessToken via the refresh token
-		refresh.requestNewAccessToken('google', googleRefreshToken, function(err, accessToken, googleRefreshToken) {
-			listGoogleDrive(accessToken, data => showPage(data, res))
-  		})
-	})
+        // Add condition to check for error to be 403. At the moment assumption is made.
+        // Get accessToken via the refresh token
+        refresh.requestNewAccessToken('google', googleRefreshToken, function(err, accessToken, googleRefreshToken) {
+            listGoogleDrive(accessToken, data => showPage(data, res))
+        })
+    })
 })
 
 app.get('/private/doc/', (req, res) => {
-	axios.get('https://www.googleapis.com/drive/v2/files', {
-		headers: { 'Authorization' : 'Bearer ' + gblAccessToken, 'Accept': 'application/json'}
-	}).then(response => {
-		console.log("*** response: " + JSON.stringify(response.data))
-		res.status(200).send('Private Docs <br /> ' + response.data)
-	}).catch(err => {
-		console.log(err)
-		res.status(200).send('Private Docs <br /> ' + err)
-	})
+    axios.get('https://www.googleapis.com/drive/v2/files', {
+        headers: { 'Authorization' : 'Bearer ' + gblAccessToken, 'Accept': 'application/json'}
+    }).then(response => {
+        logger.log('info', `*** response: ${JSON.stringify(response.data)}`)
+        res.status(200).send('Private Docs <br /> ' + response.data)
+    }).catch(err => {
+        logger.log(err)
+        res.status(200).send('Private Docs <br /> ' + err)
+    })
 })
 
 const showPage = (data, res) => {
-	console.log(`*** showPage: ${JSON.stringify(data)}`)
-	res.status(200).send(`Private Docs <br /> ${JSON.stringify(data)}`)
+    logger.log('info', `*** showPage: ${JSON.stringify(data)}`)
+    res.status(200).send(`Private Docs <br /> ${JSON.stringify(data)}`)
 }
 
 const listGoogleDrive = (accessToken, callback) => {
-	axios.get('https://www.googleapis.com/drive/v2/files', {
-		headers: { 'Authorization' : 'Bearer ' + accessToken, 'Accept': 'application/json'}
-	}).then(response => {
-		console.log("*** response: " + JSON.stringify(response.data))
-		callback(response.data)
-	}).catch(err => {
-		console.log(err)
-		return err
-	})
+    axios.get('https://www.googleapis.com/drive/v2/files', {
+        headers: { 'Authorization' : 'Bearer ' + accessToken, 'Accept': 'application/json'}
+    }).then(response => {
+        logger.log('info', '*** response: ' + JSON.stringify(response.data))
+        callback(response.data)
+    }).catch(err => {
+        logger.log('info', err)
+        return err
+    })
 }
 
 app.get('/error', (req, res) => {
-	res.status(200).send('Error Page')
+    res.status(200).send('Error Page')
 })
 
 app.get('/auth/google',
-  passport.authenticate('google', { scope: [
-	'https://www.googleapis.com/auth/plus.login',
-	// Spreadsheet permissions
-  	'https://www.googleapis.com/auth/drive',
-	'https://www.googleapis.com/auth/drive.file',
-	'https://www.googleapis.com/auth/drive.readonly',
-	'https://www.googleapis.com/auth/spreadsheets',
-	'https://www.googleapis.com/auth/spreadsheets.readonly',
-	// Drive permissions
-	'https://www.googleapis.com/auth/drive',
-	'https://www.googleapis.com/auth/drive.appdata',
-	'https://www.googleapis.com/auth/drive.apps.readonly',
-	'https://www.googleapis.com/auth/drive.file',
-	'https://www.googleapis.com/auth/drive.metadata',
-	'https://www.googleapis.com/auth/drive.metadata.readonly',
-	'https://www.googleapis.com/auth/drive.photos.readonly',
-	'https://www.googleapis.com/auth/drive.readonly'
-  	],
-  	accessType: 'offline', 
-	approvalPrompt: 'force' })
+    passport.authenticate('google', { scope: [
+        'https://www.googleapis.com/auth/plus.login',
+        // Spreadsheet permissions
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.file',
+        'https://www.googleapis.com/auth/drive.readonly',
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/spreadsheets.readonly',
+        // Drive permissions
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.appdata',
+        'https://www.googleapis.com/auth/drive.apps.readonly',
+        'https://www.googleapis.com/auth/drive.file',
+        'https://www.googleapis.com/auth/drive.metadata',
+        'https://www.googleapis.com/auth/drive.metadata.readonly',
+        'https://www.googleapis.com/auth/drive.photos.readonly',
+        'https://www.googleapis.com/auth/drive.readonly'
+    ],
+    accessType: 'offline', 
+    approvalPrompt: 'force' })
 )
 
 app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/error' }),
-  function(req, res) {
-    res.redirect('/private')
-});
+    passport.authenticate('google', { failureRedirect: '/error' }),
+    function(req, res) {
+        res.redirect('/private')
+    })
 
-app.listen(PORT, () => { console.log(`Listening on port ${PORT}`) })
+app.listen(PORT, () => { logger.info(`Listening on port ${PORT}`) })
 
 /*
  * Setup GCP Datastore
  */ 
 var datastore = new Datastore({
     projectId: projectName,
-  });
+})
 
 // Save refresh token to datastore
 const create = (profile, token) => {
-  var entity = {
-      key: datastore.key('Refresh', profile.id),
-      data: {
-        id: profile.id,
-        token: token
-      }
-  }
-
-  console.log(`entity: ${JSON.stringify(entity)}`)
-  datastore.save(entity, (err) => {
-	  console.log("Datastore save [error=" + err + "]")
-  })
+    var entity = {
+        key: datastore.key('Refresh', profile.id),
+        data: {
+            id: profile.id,
+            token: token
+        }
+    }
+  
+    logger.log('info', `entity: ${JSON.stringify(entity)}`)
+    datastore.save(entity, (err) => {
+        logger.log('info', `Datastore save [error=${err}]`)
+    })
 }
 
 //const get = uid => {
-//	const key = datastore.key(['Refresh', uid]);
-//	console.log("Search by key [key=" + key + "]")
-//	
-//	datastore.get(key)
-//  	.then(results => {
-//    		console.log(results)
-//		return results
-//  	}).catch(err => { 
-//		console.error('ERROR:', err)
-//	})
+//    const key = datastore.key(['Refresh', uid]);
+//    console.log("Search by key [key=" + key + "]")
+//    
+//    datastore.get(key)
+//      .then(results => {
+//            console.log(results)
+//        return results
+//      }).catch(err => { 
+//        console.error('ERROR:', err)
+//    })
 //}
